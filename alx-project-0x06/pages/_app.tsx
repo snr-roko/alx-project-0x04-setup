112742dbbda6e8695ea6d2ca2ec6244371a2ecd7
snr-roko <rbbagyei@outlook.com>
2025-06-15 13:03:12 +0000

    feat: integrate Redux for state management in CounterApp and Header components

--- alx-project-0x06/pages/_app.tsx
+++ alx-project-0x06/pages/_app.tsx
@@ -2,13 +2,15 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "@/components/layouts/Layout";
 import { CountProvider } from "@/context/CountContext";
+import store from "@/store/store";
+import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-  <CountProvider>
+  <Provider store={store}>
     <Layout>
       <Component {...pageProps} />
     </Layout>
-  </CountProvider>
+  </Provider>
 )
 }
